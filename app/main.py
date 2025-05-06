8aba44f13fac55097220af421bdb1a08c737739d
Бронислав <atlantwindow@gmail.com>
2025-05-06 17:18:34 +0300

    mission completed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
+import os
+import requests
+
+API_KEY = os.getenv("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    if not API_KEY:
+        print(
+            "API_KEY is not set. Make sure to provide it via environment variable."
+        )
+        exit(1)
+
+    city = "Paris"
+
+    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        weather = data["current"]["condition"]["text"]
+        temperature = data["current"]["temp_c"]
+
+        print(f"Weather in {city}: {weather}")
+        print(f"Temperature: {temperature}°C")
+    else:
+        print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
